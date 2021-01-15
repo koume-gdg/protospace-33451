e8f980f499cf9de76267a500a14416f01d606384
koume-gdg <gdgpll.vmj@gmail.com>
2021-01-15 20:26:45 +0900

    マイグレーションファイルの修正

--- db/migrate/20210112042229_add_profiles_to_users.rb
+++ db/migrate/20210112042229_add_profiles_to_users.rb
@@ -1,8 +1,8 @@
 class AddProfilesToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :user, :profile, :text
-    add_column :user, :occupation, :text
-    add_column :user, :position, :text
+    add_column :users, :profile, :text
+    add_column :users, :occupation, :text
+    add_column :users, :position, :text
 
    
   end
