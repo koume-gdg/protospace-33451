e86ce7ba2e7eea0d428ec962fd0eba1043a25a29
koume-gdg <gdgpll.vmj@gmail.com>
2021-01-18 14:19:34 +0900

    ユーザー名からの遷移修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -44,7 +44,9 @@ class PrototypesController < ApplicationController
   def destroy
     prototype = Prototype.find(params[:id])
     
-    if prototype.destroy
+    if @prototype.destroy
+      redirect_to root_path
+    else
       redirect_to root_path
     end
     
